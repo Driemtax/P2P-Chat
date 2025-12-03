85a811fb162827e3dc01898b82fb19970ee1bb02
Lars Beer <driemtax@gmail.com>
2025-12-03 17:41:02 +0100

    Outsourced Reiceved

--- cmd/chat-client/main.go
+++ cmd/chat-client/main.go
@@ -15,30 +15,14 @@ func main() {
 	printWelcome()
 	host, peer := parseCMD()
 	fmt.Println("Connecting to:", host, peer)
+	peers := make([]string, 10)
+	peers[0] = peer
 
 	go Send(peer)
+	go Reiceve(host)
 
-	addr, err := net.ResolveUDPAddr("udp", host)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ln, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ln.Close()
-
-	log.Println("Listening on ", ln.LocalAddr().String())
-
-	buf := make([]byte, maxBuffSize)
 	for {
-		// First read from the port we listen for new messages
-		size, addr, err := ln.ReadFromUDP(buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("%s: %s \n", addr, string(buf[:size]))
+
 	}
 
 }
@@ -51,13 +35,41 @@ func printWelcome() {
 }
 
 func parseCMD() (string, string) {
-	host := flag.String("port", "8080", "the port you want to listen on..")
+	host := flag.String("port", "9000", "the port you want to listen on..")
 	peer := flag.String("peer", "localhost:3000", "full remote address of the peer you want to connect to. <IP>:<PORT>")
 
 	flag.Parse()
 	port := ":" + *host
 
 	return port, *peer
+}
+
+func Reiceve(host string) {
+	addr, err := net.ResolveUDPAddr("udp", host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	log.Println("Listening on Port", host)
+
+	buf := make([]byte, maxBuffSize)
+	for {
+		// Read for new messages
+		// This is a blocking function call, so it waits till a new message arrives at the port
+		size, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("%s: %s", addr, string(buf[:size]))
+	}
+
 }
 
 func Send(peer string) {
